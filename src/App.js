5f89cc099e3eb7a4f57a6d414b274963406a6632
RCOxales <jnay.oe14@gmail.com>
2022-07-02 06:18:22 -0700

    [Feature][Rayan] add my first react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import Main from './components/Main.js'
+import Navbar from './components/navbar.js'
+
+
+
+function App(){
+  return(
+    <div className='container'>
+      <Navbar />
+      <Main /> 
+    </div>
+  )
+}
+export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,10 @@
+
+ function Main(){
+
+    return(
+
+        <h1>Main Componet</h1>
+    )
+}
+
+export default Main;
